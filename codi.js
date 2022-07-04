1664088438ef1ee4c44fe144eebb1302a7346ef3
orioltorrents <otorrents@intermunicipal.com>
2022-07-05 00:04:55 +0200

    amb tots els butons de 1ESO a BAT2

--- codi.js
+++ codi.js
@@ -10,3 +10,46 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerTutors1ESO()
+{
+    let hoja = SpreadsheetApp.openById('1YGOOBL4vuGs_dfzGSx4RjaVDijgY0k-uKxKsk0cYRLA');
+    let sheet = hoja.getSheetByName('1ESO');
+    let datos = sheet.getDataRange().getValues();
+    return datos;
+}
+function obtenerTutors2ESO()
+{
+    let hoja = SpreadsheetApp.openById('1YGOOBL4vuGs_dfzGSx4RjaVDijgY0k-uKxKsk0cYRLA');
+    let sheet = hoja.getSheetByName('2ESO');
+    let datos = sheet.getDataRange().getValues();
+    return datos;
+}
+function obtenerTutors3ESO()
+{
+    let hoja = SpreadsheetApp.openById('1YGOOBL4vuGs_dfzGSx4RjaVDijgY0k-uKxKsk0cYRLA');
+    let sheet = hoja.getSheetByName('3ESO');
+    let datos = sheet.getDataRange().getValues();
+    return datos;
+}
+function obtenerTutors4ESO()
+{
+    let hoja = SpreadsheetApp.openById('1YGOOBL4vuGs_dfzGSx4RjaVDijgY0k-uKxKsk0cYRLA');
+    let sheet = hoja.getSheetByName('4ESO');
+    let datos = sheet.getDataRange().getValues();
+    return datos;
+}
+function obtenerTutorsBAT1()
+{
+    let hoja = SpreadsheetApp.openById('1YGOOBL4vuGs_dfzGSx4RjaVDijgY0k-uKxKsk0cYRLA');
+    let sheet = hoja.getSheetByName('BAT1');
+    let datos = sheet.getDataRange().getValues();
+    return datos;
+}
+function obtenerTutorsBAT2()
+{
+    let hoja = SpreadsheetApp.openById('1YGOOBL4vuGs_dfzGSx4RjaVDijgY0k-uKxKsk0cYRLA');
+    let sheet = hoja.getSheetByName('BAT2');
+    let datos = sheet.getDataRange().getValues();
+    return datos;
+}
